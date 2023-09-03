fcb50028fd32b22ebf6ddd782126554cc2ee25c3
Invincible Inventor <invincibleinventor@gmail.com>
2023-09-03 13:06:57 +0530

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,16 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
+import { qwikCity} from "@builder.io/qwik-city/vite";
+import { qwikReact} from "@builder.io/qwik-react/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyEdge from "@netlify/vite-plugin-netlify-edge";
-import { qwikReact } from "@builder.io/qwik-react/vite";
 
 export default defineConfig(() => {
   return {
-    
-    resolve: {
-      alias: {
-        assert: "assert",
-        buffer: "buffer",
-        events: "events",
-        http: "stream-http",
-        https: "https-browserify",
-        punycode: "punycode",
-        stream: "stream-browserify",
-        url: "url",
-        util: "util",
-        zlib: "browserify-zlib",
+    plugins: [qwikCity(), qwikVite(), qwikReact(), tsconfigPaths(),],
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
       },
     },
-    
-    plugins: [
-      qwikCity(),
-      qwikVite({
-        ssr: { outDir: "netlify/edge-functions/entry.netlify-edge" },
-      }),
-      tsconfigPaths(),
-      netlifyEdge({
-        functionName: "entry.netlify-edge",
-      }),
-      qwikReact(),
-    ],
   };
 });
